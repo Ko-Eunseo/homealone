bc23df08b2ecd5609c7e7405561d0debdc576bfc
github Ko-Eunseo <github koyk0408@naver.com>
2021-12-16 23:49:10 +0900

    Fix typo

--- tomorrow-house/js/productCarousel.js
+++ tomorrow-house/js/productCarousel.js
@@ -2,11 +2,11 @@ const productCarousel = tns({
   container: '.product-carousel .slider-list',
   controls: false,
   navContainer: '.product-carousel .thumbnail-list',
-  navAsThumbnails: true,
-  arrowKeys: true,
   autoplay: true,
   autoplayHoverPause: true,
   autoplayButtonOutput: false,
+  navAsThumbnails: true,
+  arrowKeys: true,
   mouseDrag: true,
   preventScrollOnTouch: true,
 })
