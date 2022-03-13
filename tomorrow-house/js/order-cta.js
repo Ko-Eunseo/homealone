ad8ae5149585a1863bd246a065089a401f5ef156
github Ko-Eunseo <github koyk0408@naver.com>
2022-03-13 14:21:51 +0900

    Implement toggling bookmark btn

--- tomorrow-house/js/order-cta.js
+++ tomorrow-house/js/order-cta.js
@@ -16,5 +16,37 @@ function closeOrderModal() {
 }
 
 orderCtaBuyButton.addEventListener('click', openOrderModal)
-
 orderModalOverlay.addEventListener('click', closeOrderModal)
+
+
+function toggleOrderCtaBookmark() {
+  //1. 버튼 클래스: is-active
+  //2. 아이콘 클래스 변경: ic-bookmark vs. ic-bookmark-filled
+  //3. 카운트 값을 변경
+  const [icon, countSpan] = this.children //[아이콘, 카운트span]
+  const count = Number(countSpan.innerHTML.replaceAll(',', ''))
+  //console.log(typeof count) //string
+  // a.replace('b', '') a라는 값중 b는 바꾸고싶음(없애고싶을땐'') replaceAll!
+
+  let newCount = count
+
+  // is-active
+  if (this.classList.contains('is-active')) {
+    // Note: 활성화가 된 상태 ->비활성화(ic-bookmark), -1
+    icon.classList.add('ic-bookmark')
+    icon.classList.remove('ic-bookmark-filled')
+    newCount = newCount - 1
+  } else {
+    // Note: 비활성화가 된 상태 ->활성화(ic-bookmark-filled), +!
+    icon.classList.add('ic-bookmark-filled')
+    icon.classList.remove('ic-bookmark')
+    newCount = newCount + 1
+  }
+
+  countSpan.innerHTML = newCount.toLocaleString()
+  // toLocaleString(): ,넣어주는 함수
+
+  this.classList.toggle('is-active')
+}
+
+orderCtaBookmarkButton.addEventListener('click', toggleOrderCtaBookmark)
