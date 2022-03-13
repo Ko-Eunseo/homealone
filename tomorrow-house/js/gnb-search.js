a4a4d132c832981a15d4ef7dbb6b498eb94c562a
github Ko-Eunseo <github koyk0408@naver.com>
2022-03-13 16:56:01 +0900

    Implement deleting all search-histories

--- tomorrow-house/js/gnb-search.js
+++ tomorrow-house/js/gnb-search.js
@@ -1,6 +1,11 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbSearchInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
+
+const deletaAllButton = gnbSearchHistory.querySelector('.search-history-header button')
+
+console.log(deletaAllButton.innerHTML)
 
 function closeGnbSearchHistory(e) {
   if (!gnbSearch.contains(e.target)) {
@@ -10,6 +15,12 @@ function closeGnbSearchHistory(e) {
 }
 
 function openGnbSearchHistory() {
+  //체크 => gnbSearchHistoryList li가 몇개있는지
+  //li가 0개 => 실행x
+  if (gnbSearchHistoryList.children.length === 0) {
+    return
+  }
+
   if (!gnbSearchHistory.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchHistory)
   }
@@ -17,3 +28,11 @@ function openGnbSearchHistory() {
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
+
+function deleteAllSearchHistories() {
+  // gnbSearchHistoryList 안에 들어있는 모든 li 삭제
+  gnbSearchHistoryList.innerHTML = '' //빈 문자열을 넣는다
+  gnbSearchHistory.classList.remove('is-active')
+}
+
+deletaAllButton.addEventListener('click', deleteAllSearchHistories)
